14bd3eda9cf766ecae3f187d2a3d5ca9b6e9f210
Bibek Hanal <bbekhamal26@gmail.com>
2025-07-05 14:03:47 +1000

    added form and looped the data to appear in the sign up page

---
+++ src/assets/customInputs/userSignUpInputs.js
@@ -0,0 +1,44 @@
+export const singUpINputes = [
+  {
+    label: "First Name",
+    name: "fName",
+    type: "text",
+    required: true,
+    placeholder: "Bibek",
+  },
+  {
+    label: "Last Name",
+    name: "lName",
+    type: "text",
+    required: true,
+    placeholder: "Hamal",
+  },
+  {
+    label: "Email",
+    name: "email",
+    type: "text",
+    required: true,
+    placeholder: "b@b.com",
+  },
+  {
+    label: " Phone Number",
+    name: "number",
+    type: "Number",
+    required: false,
+    placeholder: "0123456789",
+  },
+  {
+    label: "Password",
+    name: "pasword",
+    type: "password",
+    required: true,
+    placeholder: "xxxx",
+  },
+  {
+    label: "Confirm Password",
+    name: "confirmpassword",
+    type: "password",
+    required: true,
+    placeholder: "xxxx",
+  },
+];
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,32 +1,24 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import CustomInput from "../../components/customInput/CustomInput.jsx";
+import { singUpINputes } from "../../assets/customInputs/userSignUpInputs.js";
 
 const SignUpPage = () => {
   return (
-    <div>
-      <p>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+    <div className="d-flex justify-content-center">
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+        <Form style={{ width: "450px"}} className="card p-5 mt-5 shadow-lg mb-5">
+          <h1>Join our Library Community</h1>
+           {
+            singUpINputes.map((input)=> (  
+            <CustomInput key = {input.name} {...input}/>))
+           }
+      
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
-      </p>
     </div>
   );
 };
