54c60c9fc429900db30c197967c20845a5e7e1b7
Bibek Hanal <bbekhamal26@gmail.com>
2025-07-24 13:31:56 +1000

    created a loginpage and backgrounf pic

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -2,31 +2,46 @@ import React from 'react'
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-const SignInPage = () => {
-  return (
-    <div>
-      <p>
-             <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+import { loginInputs } from '../../assets/customInputs/userLogInInputs.js';
+import CustomInput from '../../components/customInput/CustomInput.jsx';
+import useForm from '../../hooks/useForm.js';
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+const initialState = {}
+const SignInPage = () => {
+
+  const {form, handleOnChange} = useForm(initialState);
+  // console.log(form)
+  
+  const handleOnSubmit = (e)=> {
+    e.preventDefault();
+    console.log(form)
+  }
+
+
+  return (
+    <div className='login-page d-flex justify-content-center align-items-center'>
+
+        <Form 
+        onSubmit={handleOnSubmit} 
+        style={{width: "450px"}} className='card login-card p-5 mt-5 shadow-lg mb-5'>
+          <h1>Login to continue</h1>
+          {
+            loginInputs.map((input)=> (
+              <CustomInput key = {input.name}{...input} onChange= {handleOnChange}/>))
+      
+          }
+          <div className="py-3">
+
+          </div>
           <Button variant="primary" type="submit">
-            Submit
+            Login
           </Button>
+          <br />
+          <div className='text-center'>
+            Dont't have an account? <a href='/signup'>Signup</a> here
+            </div>
         </Form>
-      </p>
+    
     </div>
   )
 }
